54a63097b8ab5f6c85e2e8a57903b6893662f9ce
Numaan Bashir Mir <numaanbashirmir@Numaans-MacBook-Pro.local>
2024-07-20 13:37:59 +1000

    hello world

--- index.php
+++ index.php
@@ -3,14 +3,14 @@
 <link rel='stylesheet' href='picStyle.css' />
 </head>
 <body>
-
+<div class="galleryDiv">
 <?php
 $filelist = glob("./pics/*");
 
 foreach($filelist as $filename){
-	echo "<div><img src='".$filename."'></div><br>";
+	echo "<a href='galleryScript.php?picture=".$filename."'><img class ='galleryPic' src='".$filename."' class='galleryPic' /></a>";
 }
 ?>
-
+</div>
 </body>
 </html>

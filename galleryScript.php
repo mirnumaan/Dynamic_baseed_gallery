fe07aac78a1b3ffd152c3e8d2967ca7d66c7b2f3
Numaan Bashir Mir <71112748+mirnumaan@users.noreply.github.com>
2024-07-21 07:36:28 +0000

    photo added

---
+++ galleryScript.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+<link rel="stylesheet" href="picStyle.css" />
+
+</head>
+<body>
+
+<?php
+
+$image = $_GET['picture'];
+
+echo "<img class='bigPic' src='".$image."'>";
+
+?>
+
+<h1 style="text-align:center"><a href="./index.php">Gallery</a></h1>
+</body>
+</html>
--- index.php
+++ index.php
@@ -3,14 +3,17 @@
 <link rel='stylesheet' href='picStyle.css' />
 </head>
 <body>
+
 <div class="galleryDiv">
+
 <?php
 $filelist = glob("./pics/*");
 
 foreach($filelist as $filename){
-	echo "<a href='galleryScript.php?picture=".$filename."'><img class ='galleryPic' src='".$filename."' class='galleryPic' /></a>";
+	echo "<a href='galleryScript.php?picture=".$filename."'><img class='galleryPic' src='".$filename."'></a>";
 }
 ?>
+
 </div>
 </body>
 </html>
